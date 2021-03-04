e33e340473226141f0090aa8e2ed50687aee4830
Alvin Wong <wong.alvin13@gmail.com>
2021-03-04 14:50:33 -0600

    category changed to get from db

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -5,22 +5,27 @@ function FavoritesList() {
   const dispatch = useDispatch();
 
   // categories
-  const [newCategory, setNewCategory] = useState('')
+  const [newCategory, setNewCategory] = useState(0)
 
   // get data from store for list
   const favoriteList = useSelector(store => store.Reducer);
   // get data from store for categories
+  const categories = useSelector(store => store.categoryReducer);
 
   useEffect(() => {
     dispatch({
       type: 'GET_FAVORITES'
+    });
+    dispatch({
+      type: 'GET_CATEGORY'
     });
   }, []);
 
   const changeCategory = (id) => {
     // send category to saga then to DB
     const newFavorite = {
-
+        category_id: Number(newCategory),
+        favorite_id: Number(id)
     }
     dispatch({
       type: 'CHANGE_CATEGORY',
@@ -40,11 +45,9 @@ function FavoritesList() {
             name="categories" 
             onChange={(event) => setNewCategory(event.target.value)}
           >
-            <option>Funny</option>
-            <option>Cohort</option>
-            <option>Cartoon</option>
-            <option>NSFW</option>
-            <option>Meme</option>
+            {categories.map(category => {
+              <option value={category.id}>{category.name}</option>
+            })}
           </select>
           <div>
             <button onClick={() => {changeCategory(favorite.id)}}>Change Category</button>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const sagaMiddleware = createSagaMiddleware();
+
+//categoryReducer
+const categoryReducer = (state = [], action) => {
+    if(action.type === 'SEND_CAT_TO_REDUCER') {
+      return action.payload;
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+  combineReducers({
+    categoryReducer,
+    // reducers here
+  }),
+
+  applyMiddleware(sagaMiddleware, logger)
+);
 
 ReactDOM.render(
-<App />,
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
 document.getElementById('root'));
